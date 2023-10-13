888c907e3e7f557d394cfbc83ac8ce2e81f10088
interfacetuner <damianchankins@gmail.com>
2023-10-13 11:42:38 -0400

    second commit

--- picScraper.py
+++ picScraper.py
@@ -5,7 +5,7 @@ def getdata(url):
     r = requests.get(url)  
     return r.text  
     
-htmldata = getdata("https://www.geeksforgeeks.org/")  
+htmldata = getdata("https://www.google.com/")  
 soup = BeautifulSoup(htmldata, 'html.parser')  
 for item in soup.find_all('img'): 
     print(item['src'])
