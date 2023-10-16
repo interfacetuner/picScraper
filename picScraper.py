a37797e9707eade4df1136d0d156f2ab715e998e
interfacetuner <damianchankins@gmail.com>
2023-10-16 15:46:32 -0400

    comment

--- picScraper.py
+++ picScraper.py
@@ -9,3 +9,7 @@ htmldata = getdata("https://www.google.com/")
 soup = BeautifulSoup(htmldata, 'html.parser')  
 for item in soup.find_all('img'): 
     print(item['src'])
+
+    '''
+    This script will scrape all the images from a given URL using Python and Beautiful Soup library.
+    '''
